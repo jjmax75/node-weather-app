ffcc6ddcbe8c4efe56d28e9ccacc5565579e9088
John Behan <behanj@gmail.com>
2019-07-09 23:16:08 +0100

    play around with response

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const request = require('request');
 
 const apiKey = process.env.DARK_SKY_API_KEY;
 
-const url = `https://api.darksky.net/forecast/${apiKey}/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/${apiKey}/53.162222,-7.191111?units=si`;
 
-request({ url }, (error, response) => {
-	const data = JSON.parse(response.body);
-	console.log(data.currently);
+request({ url, json: true }, (error, response) => {
+	const description = response.body.daily.data[0].summary;
+	const { temperature, precipProbability } = response.body.currently;
+	console.log(
+		`${description} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`,
+	);
 });
