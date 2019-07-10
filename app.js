1b6dcc3c31e2c705bbf02a6f5b9aad92ace18d5a
John Behan <behanj@gmail.com>
2019-07-10 16:58:01 +0100

    setup loaction service

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ require('dotenv').config();
 const request = require('request');
 
 const apiKey = process.env.DARK_SKY_API_KEY;
+const locationServiceApiKey = process.env.MAPBOX_API_KEY;
 
-const url = `https://api.darksky.net/forecast/${apiKey}/53.162222,-7.191111?units=si`;
+const locationServiceUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${locationServiceApiKey}`;
 
-request({ url, json: true }, (error, response) => {
+const weatherServiceUrl = `https://api.darksky.net/forecast/${apiKey}/53.162222,-7.191111?units=si`;
+
+request({ url: weatherServiceUrl, json: true }, (error, response) => {
 	const description = response.body.daily.data[0].summary;
 	const { temperature, precipProbability } = response.body.currently;
 	console.log(
 		`${description} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`,
 	);
+});
+
+request({ url: locationServiceUrl, json: true }, (error, response) => {
+	const coords = {
+		long: response.body.features[0].center[0],
+		lat: response.body.features[0].center[1],
+	};
+	const locationName = response.body.features[0].place_name;
+	console.log(`${locationName} is at ${coords.lat}, ${coords.long}.`);
 });
